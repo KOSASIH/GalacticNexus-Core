14190d74e6849607c348e895f931caa9cc8e08c6
KOSASIH <kosasihg88@gmail.com>
2024-07-06 15:39:11 +0700

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,5 @@
+export const GALACTIC_NEXUS_CORE_VERSION = '1.0.0';
+export const BLOCKCHAIN_NETWORK_ID = 'galactic-nexus-core';
+export const AI_MODEL_VERSION = 'neural_network_v2';
+export const QUANTUM_COMPUTING_PROVIDER = 'Qiskit';
+export const NEURO_INTERFACE_PROTOCOL = 'BCI-v1';
