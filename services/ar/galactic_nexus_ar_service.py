3a21e7acdaa17bbddc020444ce22e210b2b48284
KOSASIH <kosasihg88@gmail.com>
2024-07-06 16:30:26 +0700

    Create galactic_nexus_ar_service.py

---
+++ services/ar/galactic_nexus_ar_service.py
@@ -0,0 +1,13 @@
+import ARCore
+
+class GalacticNexusAR:
+    def __init__(self):
+        self.session = ARCore.Session()
+
+    def create_anchor(self, anchor_name):
+        # Create anchor in AR space
+        pass
+
+    def track_anchor(self, anchor_name):
+        # Track anchor in AR space
+        pass
