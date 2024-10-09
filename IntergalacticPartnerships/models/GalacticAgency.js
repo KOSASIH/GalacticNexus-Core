048731cb5b9c9365eb4e0e50590f0c9ad0213760
KOSASIH <kosasihg88@gmail.com>
2024-10-09 13:36:07 +0700

    Create GalacticAgency.js

---
+++ IntergalacticPartnerships/models/GalacticAgency.js
@@ -0,0 +1,9 @@
+export class GalacticAgency {
+  constructor(id, name, description, logo, website) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.logo = logo;
+    this.website = website;
+  }
+}
