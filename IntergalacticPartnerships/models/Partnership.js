420531e17cde0efd23e32bebea946bc88f23627c
KOSASIH <kosasihg88@gmail.com>
2024-10-09 13:37:07 +0700

    Create Partnership.js

---
+++ IntergalacticPartnerships/models/Partnership.js
@@ -0,0 +1,9 @@
+export class AlienCivilization {
+  constructor(id, name, description, logo, website) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.logo = logo;
+    this.website = website;
+  }
+}
