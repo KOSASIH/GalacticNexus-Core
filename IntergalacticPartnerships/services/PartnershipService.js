af13c10e402e7b3d872d4591b8544667f2e507d4
KOSASIH <kosasihg88@gmail.com>
2024-10-09 13:40:38 +0700

    Create PartnershipService.js

---
+++ IntergalacticPartnerships/services/PartnershipService.js
@@ -0,0 +1,25 @@
+import { GalacticAgency } from '../models/GalacticAgency';
+import { AlienCivilization } from '../models/AlienCivilization';
+import { Partnership } from '../models/Partnership';
+
+export class PartnershipService {
+  async getAllPartnerships() {
+    // TO DO: implement logic to retrieve all partnerships
+  }
+
+  async getPartnershipById(id) {
+    // TO DO: implement logic to retrieve a partnership by ID
+  }
+
+  async createPartnership(partnershipData) {
+    // TO DO: implement logic to create a new partnership
+  }
+
+  async updatePartnership(id, partnershipData) {
+    // TO DO: implement logic to update a partnership
+  }
+
+  async deletePartnership(id) {
+    // TO DO: implement logic to delete a partnership
+  }
+}
