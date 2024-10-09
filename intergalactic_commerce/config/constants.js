90acd9497b0e4c70dca8b871d36a3638d6fce937
KOSASIH <kosasihg88@gmail.com>
2024-10-09 21:16:39 +0700

    Create constants.js

---
+++ intergalactic_commerce/config/constants.js
@@ -0,0 +1,6 @@
+export const GNX_TOKEN_SYMBOL = 'GNX';
+export const GNX_TOKEN_DECIMALS = 18;
+export const GALACTIC_CURRENCY_SYMBOL = 'GAL';
+export const INTERGALACTIC_TRANSACTION_FEE = 0.01;
+export const PLANETARY_SYSTEM_CATEGORIES = ['Terrestrial', 'Gas Giant', 'Ice Giant', 'Dwarf'];
+export const CIVILIZATION_TYPES = ['Humanoid', 'Reptilian', 'Insectoid', 'Energy Being'];
