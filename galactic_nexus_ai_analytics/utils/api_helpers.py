91b34e3da148c9e6c64bf18337da882c918016bc
KOSASIH <kosasihg88@gmail.com>
2024-10-11 00:17:10 +0700

    Create api_helpers.py

---
+++ galactic_nexus_ai_analytics/utils/api_helpers.py
@@ -0,0 +1,9 @@
+def get_asset_balance(address):
+    # Get asset balance from blockchain
+    # ...
+    return balance
+
+def get_market_data():
+    # Get market data from external API
+    # ...
+    return market_data
