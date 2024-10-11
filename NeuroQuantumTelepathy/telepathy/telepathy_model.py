66747c780e89010b9afb2a97f11cf5fc008489cb
KOSASIH <kosasihg88@gmail.com>
2024-10-11 17:28:10 +0700

    Create telepathy_model.py

---
+++ NeuroQuantumTelepathy/telepathy/telepathy_model.py
@@ -0,0 +1,17 @@
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, LSTM
+
+class TelepathyModel:
+  def __init__(self, config):
+    self.config = config
+    self.build_model()
+
+  def build_model(self):
+    input_layer = Input(shape=self.config["input_shape"])
+    x = LSTM(128, return_sequences=True)(input_layer)
+    x = LSTM(64)(x)
+    output_layer = Dense(self.config["output_shape"][0], activation='softmax')(x)
+    self.model = Model(inputs=input_layer, outputs=output_layer)
+    self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    self.model.save(self.config["model_path"])
