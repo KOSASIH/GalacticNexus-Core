e690396f20f4f6de059478725037b7f8df2856d1
KOSASIH <kosasihg88@gmail.com>
2024-10-13 20:34:27 +0700

    Delete tests/setup.py

--- tests/setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="galacticnexus-core-testing",
-    version="1.0",
-    packages=["galacticnexus_core"],
-    install_requires=["pytest", "pytest-html"]
-)
