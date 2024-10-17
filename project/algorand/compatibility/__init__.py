47ba41c1ce64326cb3b377b7d76f369faeb4ecab
KOSASIH <kosasihg88@gmail.com>
2024-10-17 20:53:27 +0700

    Create __init__.py

---
+++ project/algorand/compatibility/__init__.py
@@ -0,0 +1,7 @@
+# project/algorand/compatibility/__init__.py
+
+"""
+Compatibility Module for Galactic Nexus
+
+This package contains tools and scripts for checking compatibility with various versions of the Algorand SDK and other dependencies.
+"""
