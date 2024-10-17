ddcc7f91f3d9400cacd3f0d5d623ee0f427275d1
KOSASIH <kosasihg88@gmail.com>
2024-10-17 20:47:41 +0700

    Create __init__.py

---
+++ project/algorand/security/__init__.py
@@ -0,0 +1,7 @@
+# project/algorand/security/__init__.py
+
+"""
+Security Module for Galactic Nexus
+
+This package contains tools and scripts for conducting security audits and ensuring the integrity of the codebase.
+"""
